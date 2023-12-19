92dc5ee697c10da70fdd135d6624c9560762bda4
Sergey <sergeinickit@gmail.com>
2023-12-19 13:44:41 +0300

    добавил Example005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")               //ToLower - переводит в нижний регистр и устраняет проблемы с написанием(маша, мАша, МаШа и тд)
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

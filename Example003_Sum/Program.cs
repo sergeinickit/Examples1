c176c0d930845e24f22d0b8143f64983d3fe036a
Sergey <sergeinickit@gmail.com>
2023-12-19 13:04:28 +0300

    добавил файлы

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -3,7 +3,14 @@
 // Console.WriteLine(numberA + numberB);
 
 // Или так:
-int numberA = 31;
-int numberB = 5;
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);

ec572837ed569e5c9870cabf00e60fb48d333062
Sergey <sergeinickit@gmail.com>
2023-12-19 12:38:26 +0300

    добавил Example003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+// Или так:
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
